8b42dad543d5bf2d0b52741d5679f86a80d80d31
fergalswaine <113620944+fergalswaine@users.noreply.github.com>
2024-10-16 15:10:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerMenu = document.querySelector(".hamburger-icon");
+    const menuLinks = document.querySelector(".menu-links");
+  
+    hamburgerMenu.addEventListener("click", function () {
+      menuLinks.classList.toggle("open");
+    });
+  });
+  
+  document.getElementById("dark-mode-toggle").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+    const isDarkMode = document.body.classList.contains("dark-mode");
+    this.innerText = isDarkMode ? "Toggle Light Mode" : "Toggle Dark Mode";
+});
